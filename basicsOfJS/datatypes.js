5b9c6077e4d3236b1d9ea59edd7017b93d968a4e
souravwhitehat <76489822+souravwhitehat@users.noreply.github.com>
2025-05-31 05:51:17 +0000

    datatypes&variable done

---
+++ basicsOfJS/datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+
+console.log("variables");
+console.log("jbhjbkjb");
+let name = "sourav"//string
+let age = 18; // number
+let isLogged = true ; // boolean
+let accountNo = 1012510524514210121401511 // bigint
+let address = null; // null type
+let accountName;// undefined 
+let sym = Symbol('foo'); //symbol
+console.table([name , age , isLogged , accountName , accountNo,sym ])
+
+
+console.log("object");
+console.log(typeof age)
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
---
+++ basicsOfJS/variables.js
@@ -0,0 +1,12 @@
+console.log('sourav')
+
+const accountId = 1122342;//we can naot change the const value
+var accountName =  "Sourav"
+let accoutnEmail = "sourav@gmail.com"
+accountCity = "Bhubaneswar"
+
+//we can change var but in some case we can not change let .....
+ let accountState;// undefined
+console.log(accountName , accountId , accoutnEmail , accountCity)
+console.table([accountName , accountId , accoutnEmail , accountCity , accountState])
+ 
