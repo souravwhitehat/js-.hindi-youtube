8318ce92a4cbf30ad12bd8897295e2e5be6bec59
souravwhitehat <76489822+souravwhitehat@users.noreply.github.com>
2025-06-01 04:57:25 +0000

    datatype summaty done

---
+++ basicsOfJS/comparision.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+const { StrictMode } = require("react");
+
+ 
+
+// console.log("2">1);
+// console.log("sa2">1);
+
+//avoid this type of conversions.....
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >=0);
+
+// ===
+console.log("2" ===2); //Strictly check both value and data type
+console.log("2"== 2);//only check the value not data type
+
+
+
+//done
--- basicsOfJS/datatype_conversion.js
+++ basicsOfJS/datatype_conversion.js
@@ -14,8 +14,8 @@ console.log(valueInNumber);
 
 let booleanInNumber = "hii";
 
-let numberInBoolean = Boolean(booleanInNumber);
-console.log(numberInBoolean)
+// let numberInBoolean = Boolean(booleanInNumber);
+// console.log(numberInBoolean)
 
 // 1=true
 //0 = false
@@ -26,3 +26,91 @@ let numTostring = 12;
 let stringToNumber = (String(numTostring));
 console.log(stringToNumber);
 console.log(typeof stringToNumber);
+
+
+// ***********************************************operations**************************************************************************
+
+let value = 3;
+let negvalue = -value;
+console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2%2)
+// console.log(2/2)
+
+
+
+//string Concardinations
+let str1 = "hii";
+let str2 = " sourav";
+let str3= str1 + str2;
+
+// console.log(str3);
+
+// console.log("2"+2);
+// console.log("2"+2+2);
+// console.log(2+2+"2");
+// console.log(2+"2"+(2+2));//number+ string = string.....
+
+
+// ********************tricky conversions*************
+// console.log(true)//true
+// console.log(+true);//1
+// console.log(+"");//0
+
+
+let num1 , num2 , num3;
+num1 = num2 = num3 = 2+2;
+console.log(num1 , num2 , num3);
+
+
+let num4 = 100;
+num4++;
+console.log(num4);
+//link to study .........
+//   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+// ****************DataTypes******************
+//Primitive
+
+// 7types : String , Number , Boolean, null , Undefined , Bigint , Symbol.........
+
+const score =100;
+let getBoolean = true;
+let id = Symbol("hooo")
+let anotherId = Symbol("hooo")
+console.log(id == anotherId);
+
+
+// JS is dynamically typed cause you don’t need to declare the type of a variable.
+
+//Reference Type or Non primitive
+// Array , Object , Function
+// Array
+const fruits =["apple", "banana", "orange"];
+console.log(typeof fruits)
+// Object
+let myObj = {
+    name : "sourav",
+    class  : "MCA",
+    age : 20
+}
+// console.table(myObj)
+console.log(myObj);
+
+// Function
+const myFunc = function(){
+    console.log("hii")
+}
+myFunc();
+
+
+
+
+
+
+
