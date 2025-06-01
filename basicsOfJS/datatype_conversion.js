b1ad2a229004b48ec62176926863efc903671736
souravwhitehat <76489822+souravwhitehat@users.noreply.github.com>
2025-06-01 02:58:17 +0000

    datatype conversion done

---
+++ basicsOfJS/datatype_conversion.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let num = undefined;
+let valueInNumber = Number(num);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+// "33"= 33
+// True = 1
+// false = 0
+// Null = 0;
+// undefined = nan;
+
+let booleanInNumber = "hii";
+
+let numberInBoolean = Boolean(booleanInNumber);
+console.log(numberInBoolean)
+
+// 1=true
+//0 = false
+//""= false
+//"hii" = true
+
+let numTostring = 12;
+let stringToNumber = (String(numTostring));
+console.log(stringToNumber);
+console.log(typeof stringToNumber);
