42828af63a39610b9696e8744f31b45167170f23
sourav kumar biswal <76489822+souravwhitehat@users.noreply.github.com>
2025-07-01 17:50:49 +0000

    for loops

---
+++ controlFlow/loops.js
@@ -0,0 +1,48 @@
+console.log("loops...............iteration......😎");
+
+// for (let i = 0; i <=10; i++) {
+//    console.log(i);
+//    if(i===5){
+//     console.log(`${i} is my birthday...💕 happy birthday`);
+    
+//    }
+   
+    
+// }
+
+for (let i = 1; i <= 10; i++) {
+  //  console.table(`${i}th table`);   
+    for (let j = 1; j <=10; j++) {
+        //console.table(`${i} * ${j} = ${i*j}`);
+        
+    }
+}
+
+let myArr = ["mango" , "banana" , "orrange"];
+for (let i = 0; i <= myArr.length; i++) {
+    const element = myArr[i];
+    // console.log(element);
+    
+}
+
+// Break and Continue
+for (let i =1; i <=20; i++) {
+   
+    if (i===5) {
+        // console.log(`${i} is detected`) ;
+        break
+    }
+    // console.log(i.);
+        
+}
+
+//continue
+for (let i =1; i <=20; i++) {
+     
+    if (i===5) {
+        // console.log(`${i} is detected`) ;
+        continue
+    }
+    // console.log(i);
+        
+}
