24eb491332ab593919ff036f668415adbafa1371
sourav kumar biswal <76489822+souravwhitehat@users.noreply.github.com>
2025-07-02 05:20:47 +0000

    whileloop & dowhileloop over

---
+++ controlFlow/while.js
@@ -0,0 +1,25 @@
+console.log("while...........");
+
+// let i  = 0;
+// while (i<=10) {
+//     console.log("hii....");
+//     i++;
+
+// }
+
+// let j = 0;
+// const myArr = ["sourav", "rashmi ", "milan"];
+// while (j<= myArr.length) {
+//     console.log(myArr[j]);
+//     j++;
+
+    
+// }
+
+// do while loop
+let score = 11;
+
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score<=10);
